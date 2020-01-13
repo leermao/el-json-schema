0567dfc837145cd5734120eb80847de84a4cc3c5
lipeng1 <lipeng1@360.cn>
2020-01-13 11:47:44 +0800

    init

--- src/formschema.js
+++ src/formschema.js
@@ -1,9 +1,56 @@
-import FormsShema from './formschema'
-
-const install = function installFormsShema (Vue) {
-  Vue.component('el-form-shema', FormsShema)
-}
-
 export default {
-  install
-}
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  data() {
+    return {
+      formData: {}
+    }
+  },
+
+  render(h) {
+    const vm = this
+
+    return h(
+      'el-form',
+      {
+        props: {}
+      },
+      [
+        ...(vm.$slots.prepend || []),
+        ...(vm.renderFormItems(h) || []),
+        ...(vm.$slots.append || [])
+      ]
+    )
+  },
+
+  methods: {
+    renderFormItem(h) {
+      const children = [];
+      const tagsMap = {
+        'el-input': handleRenderInput,
+        'el-select': handleRenderSelect,
+        'el-checkbox': handleCheckBox
+      }
+
+      this.value.map(item => {
+        if (tagsMap[item.tag]) {
+          const formItem = h(
+            'el-form-item',
+            {
+              props: {}
+            },
+            tagsMap[item.tag](h, item)
+          );
+          children.push(formItem)
+        }
+      });
+
+      return children;
+    }
+  }
+};
