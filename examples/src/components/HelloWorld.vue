7ed1851b3d44d8c6e664f4e5bba8a9265ee5faa9
lipeng1 <lipeng1@360.cn>
2020-01-13 17:37:10 +0800

    input

--- examples/src/components/HelloWorld.vue
+++ examples/src/components/HelloWorld.vue
@@ -1,13 +1,46 @@
 <template>
-  <el-form-shema :schema="schema"></el-form-shema>
+  <el-form-shema
+    :schemas="schemas"
+    v-model="formValues"
+    @submit="handleSubmit"
+  ></el-form-shema>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      schema: []
+      schemas: [
+        {
+          tag: "el-input",
+          props: {
+            placeholder: "输入placeholder",
+            clearable: true,
+            "show-password": true,
+            // disabled: true,
+            size: "small"
+          },
+          events: {
+            blur() {
+              console.log("事件");
+            }
+          },
+          rule: [],
+          render: () => {},
+          options: [],
+          model: "name",
+          label: "输入框"
+        }
+      ],
+      formValues: {
+        name: "Some Awesome Title"
+      }
     };
+  },
+  methods: {
+    handleSubmit(data) {
+      console.log(data);
+    }
   }
 };
 </script>
--- examples/src/util/formschema.js
+++ examples/src/util/formschema.js
@@ -1,66 +1,150 @@
-// import { hasChildren } from "./utils";
-
 export default {
-  name: "formJsonSchema",
+  name: "formJsonSchemas",
   props: {
-    schema: {
+    schemas: {
       type: Array,
       default: () => []
+    },
+    value: {
+      type: Object,
+      default: () => {}
     }
   },
   data() {
     return {
-      formData: {}
+      formData: this.mergeValues()
     };
   },
   render(h) {
     return h(
       "el-form",
       {
-        props: {}
+        props: {
+          model: this.formData,
+          labelWidth: "80px"
+        },
+        ref: "formData"
       },
-      [...this.renderFormItems(h)]
+      [...this.renderFormItems(h), ...this.renderButtomItems(h)]
     );
   },
-  methods: {
-    renderFormItems(h) {
-      return [h("div", 111), h("span", 333)];
+
+  created() {
+    this.$emit("input", this.formData);
+  },
+
+  watch: {
+    formData: {
+      handler(formData) {
+        this.updating = true;
+        this.$emit("input", formData);
+      },
+      deep: true
     }
-    // /**
-    //  * el-form-item
-    //  */
-    // renderFormItem(h) {
-    //   const children = [];
-    //   const tagsMap = {
-    //     "el-input": this.handleRenderInput,
-    //     "el-select": this.handleRenderSelect,
-    //     "el-checkbox": this.handleRenderCheckBox
-    //   };
-    //   this.schema.map(item => {
-    //     if (tagsMap[item.tag]) {
-    //       const formItem = h(
-    //         "el-form-item",
-    //         {
-    //           props: {}
-    //         },
-    //         tagsMap[item.tag](h, item)
-    //       );
-    //       children.push(formItem);
-    //     }
-    //   });
-    //   return children;
-    // },
-    // /**
-    //  * 输入框
-    //  */
-    // handleRenderInput() {},
-    // /**
-    //  * 选择框
-    //  */
-    // handleRenderSelect() {},
-    // /**
-    //  * 复选框
-    //  */
-    // handleRenderCheckBox() {}
+  },
+
+  methods: {
+    mergeValues() {
+      const vm = this;
+      const { value } = vm;
+      const defaultValues = {};
+
+      return {
+        ...defaultValues,
+        ...value
+      };
+    },
+    renderButtomItems(h) {
+      const vm = this;
+      return [
+        h(
+          "el-button",
+          {
+            on: {
+              click() {
+                vm.$refs.formData.resetFields();
+                vm.$emit("cancel", { ...vm.formData });
+              }
+            }
+          },
+          "取消"
+        ),
+        h(
+          "el-button",
+          {
+            on: {
+              click() {
+                vm.$emit("submit", { ...vm.formData });
+              }
+            }
+          },
+          "确定"
+        )
+      ];
+    },
+    /**
+     * el-form-item
+     */
+    renderFormItems(h) {
+      const vm = this;
+      const children = [];
+      const tagsMap = {
+        "el-input": vm.handleRenderInput,
+        "el-select": vm.handleRenderSelect,
+        "el-checkbox": vm.handleRenderCheckBox
+      };
+
+      vm.schemas.map(item => {
+        if (tagsMap[item.tag]) {
+          const formItem = h(
+            "el-form-item",
+            {
+              props: {
+                rule: [...item.rule],
+                label: item.label
+              }
+            },
+            [...tagsMap[item.tag](h, item)]
+          );
+
+          children.push(formItem);
+        }
+      });
+
+      return children;
+    },
+    /**
+     * 输入框
+     */
+    handleRenderInput(h, item) {
+      const vm = this;
+      const value = vm.formData[item.model] || null;
+      const modelEvents = {
+        input: function(value) {
+          vm.formData[item.model] = value;
+        }
+      };
+
+      return [
+        h(item.tag, {
+          props: {
+            value,
+            ...item.props
+          },
+          on: {
+            ...modelEvents,
+            ...item.events
+          }
+        })
+      ];
+    },
+    /**
+     * 选择框
+     */
+    handleRenderSelect() {},
+    /**
+     * 复选框
+     */
+    handleRenderCheckBox() {}
   }
 };
