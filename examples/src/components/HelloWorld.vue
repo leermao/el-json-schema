be5c31e2593911392f416b14daf42ce4beb09470
lipeng1 <lipeng1@360.cn>
2020-01-14 11:04:35 +0800

    init

--- examples/src/components/HelloWorld.vue
+++ examples/src/components/HelloWorld.vue
@@ -1,130 +1,178 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <el-form-shema
+    :schemas="schemas"
+    v-model="formValues"
+    @submit="handleSubmit"
+  ></el-form-shema>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      schemas: [
+        {
+          tag: "el-input",
+          props: {
+            size: "small",
+            placeholder: "测试输入框"
+          },
+          events: {
+            blur() {}
+          },
+          rule: [
+            { required: true, message: "请输入邮箱地址", trigger: "blur" },
+            {
+              type: "email",
+              message: "请输入正确的邮箱地址",
+              trigger: ["blur", "change"]
+            }
+          ],
+          render: () => {},
+          options: [],
+          model: "name",
+          label: "输入框"
+        },
+        {
+          tag: "el-select",
+          props: {
+            size: "small",
+            multiple: true,
+            placeholder: "测试输入框为数组的情况"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: [
+            {
+              value: "选项1",
+              label: "黄金糕"
+            },
+            {
+              value: "选项2",
+              label: "双皮奶"
+            },
+            {
+              value: "选项3",
+              label: "蚵仔煎"
+            },
+            {
+              value: "选项4",
+              label: "龙须面"
+            },
+            {
+              value: "选项5",
+              label: "北京烤鸭"
+            }
+          ],
+          model: "select1",
+          label: "选择框1"
+        },
+        {
+          tag: "el-select",
+          props: {
+            size: "small",
+            placeholder: "测试输入框为对象的情况"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: {
+            1: "选项1",
+            2: "选项2",
+            3: "选项3"
+          },
+          model: "select2",
+          label: "选择框2"
+        },
+        {
+          tag: "el-date-picker",
+          props: {
+            size: "small",
+            placeholder: "日期选择"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: [],
+          model: "picker",
+          label: "日期选择"
+        },
+        {
+          tag: "el-switch",
+          props: {
+            size: "small",
+            placeholder: "日期选择"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: [],
+          model: "switch",
+          label: "开关"
+        },
+        {
+          tag: "el-radio",
+          props: {
+            placeholder: "单选框"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: ["上海", "北京", "广州", "深圳"],
+          model: "radio",
+          label: "单选框Array"
+        },
+        {
+          tag: "el-radio",
+          props: {
+            placeholder: "单选框"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: ["北京", "广州", "深圳"],
+          model: "radio1",
+          label: "单选框Array-OBJ"
+        },
+        {
+          tag: "el-checkbox",
+          props: {
+            size: "small",
+            placeholder: "复选框"
+          },
+          events: {},
+          rule: [],
+          render: () => {},
+          options: ["上海", "北京", "广州", "深圳"],
+          model: "checkbox",
+          label: "复选框Array"
+        },
+        {
+          tag: "el-custom",
+          render: h => {
+            return h("div", 111);
+          },
+          model: "custom",
+          label: "自定义"
+        }
+      ],
+      formValues: {
+        name: "",
+        select1: "",
+        select2: "",
+        picker: "",
+        switch: "",
+        radio: "",
+        radio1: "",
+        checkbox: "",
+        checkbox1: "",
+        checkbox2: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {}
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
