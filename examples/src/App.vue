74a45b0be6133ae9618b7dff0ae73370398615f1
lipeng1 <lipeng1@360.cn>
2020-01-14 11:59:40 +0800

    demo

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">基础用法</router-link> |
+      <router-link to="/about">UI界面快速生成</router-link>
     </div>
     <router-view />
   </div>
@@ -18,6 +18,7 @@
 
 #nav {
   padding: 30px;
+  text-align: center;
 
   a {
     font-weight: bold;
--- examples/src/views/About.vue
+++ examples/src/views/About.vue
@@ -1,5 +1,66 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-card class="box-card">
+      <div class="setting">设置</div>
+      <el-form ref="form" :model="form" label-width="150px" size="small">
+        <el-col :span="8">
+          <el-form-item label="表单类型">
+            <el-radio-group v-model="form.type">
+              <el-radio label="列表查询" :value="1"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="8">
+          <el-form-item label="是否行内布局">
+            <el-switch v-model="form.inline" active-color="#13ce66">
+            </el-switch>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="12">
+          <el-form-item label="label-width" style="width:100%">
+            <el-slider v-model="form.labelWidth" :max="200"></el-slider>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="12">
+          <el-form-item label="component-width" style="width:100%">
+            <el-slider v-model="form.componentWidth" :max="24"></el-slider>
+          </el-form-item>
+        </el-col>
+      </el-form>
+    </el-card>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        type: "1",
+        inline: true,
+        labelWidth: 80,
+        componentWidth: 6
+      }
+    };
+  }
+};
+</script>
+<style lang="less">
+.setting {
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 10px;
+    height: 24px;
+    background: red;
+    margin-right: 10px;
+  }
+}
+</style>
