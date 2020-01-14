e5b6e53b0fb19b866d4a78e6d334b06c150a4f0a
lipeng1 <lipeng1@360.cn>
2020-01-14 11:06:47 +0800

    init

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,12 +1,17 @@
 import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
+
+Vue.use(ElementUI);
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+});
